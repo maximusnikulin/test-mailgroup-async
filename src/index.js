05b4966d509daf3e94187811eeb50732bb825446
Max Nikulin <perseusxii@gmail.com>
2017-10-20 14:35:29 +0300

    init repository

---
+++ src/index.js
@@ -0,0 +1,30 @@
+function asyncForEach (arr, fn) {  
+    var defaultLog  = console.log;
+    var customLog = function() {
+        var args = [].slice.call(arguments, 0);
+        var str = args[0];            
+        str = "Processing " + str.charAt(0).toLowerCase() + str.slice(1);
+        args[0] = str;
+        return defaultLog.apply(this, args);
+    };
+    return new Promise(function(res, rej) {            
+        var i = 0;            
+        var next = function(i) {
+            if (i == arr.length - 1) {                    
+                res();                    
+            } else {
+                fn(arr[i], i, () => next(i));
+                i += 1;
+            }
+        }            
+        setTimeout(() => {
+            console.log = customLog;
+            next(i);
+        }, 0);
+    })
+    .then(() => new Promise(function(res,rej) {
+       console.log =  defaultLog;
+       res();
+    }));        
+                
+}
---
+++ test/index.js
@@ -0,0 +1,9 @@
+console.log('Before');
+
+asyncForEach([1,2,3], function (item, index, next) {      
+  console.log('Item %s at %s', item, index);
+  setTimeout(next, 10);    
+})
+.then(() => console.log("Done"))
+
+console.log('After');
